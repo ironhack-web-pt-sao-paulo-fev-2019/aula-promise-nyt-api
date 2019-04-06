f51559fc0ae09da8270a6d49e8095ba3b00bcabd
Sany Chernizon <chernizon@gmail.com>
2019-04-06 16:26:03 -0300

    finalizado

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const hbs = require('hbs');
 const app = express();
 const fetch = require('node-fetch');
 
+require('dotenv').config();
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-
+app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
-
-const apiKey = '6tsbZLntMYIzy0BEidlmbQGzjSmlTA8x'
 
 app.get('/', (request, response) => {
     response.render('index');
 })
 
 app.get('/search-results', (request, response) => {
-    fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=6tsbZLntMYIzy0BEidlmbQGzjSmlTA8x')
+    fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + process.env.API_KEY + '&q=' + request.query.q)
         .then(res => res.json())
         .then(json => {
-            console.log(json.response.docs)
             let data = json.response.docs;
             response.render('search', { data });
         });
