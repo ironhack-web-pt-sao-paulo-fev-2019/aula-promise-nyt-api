a0d77dc859976713c4fe24bff39d32a8141acb4a
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-04-06 21:07:54 -0300

    ajustes no css, criação de novas partials e alterações no layout

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const app = require('./routes');
 require('dotenv').config();
 
-console.log(process.env.PORT_APP)
 app.listen(process.env.PORT_APP, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
